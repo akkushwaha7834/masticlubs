b21aac6056721f57de7b0fc162d4d61e901a697e
Amit Kushwaha <akkushwaha018@gmail.com>
2024-04-03 20:29:12 +0530

    header-footer-seperated

---
+++ includes/footer.php
@@ -0,0 +1,161 @@
+
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-6"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-5 footer_menu_list">
+          <ul class="footer-menu">
+            <li>
+              <a href="#" class="text-decoration-none text-white">Home</a>
+            </li>
+            <span class=" text-white">|</span>
+            <li>
+              <a href="#" class="text-decoration-none text-white">Contact Us</a>
+            </li>
+            <span class=" text-white">|</span>
+            <li>
+              <a href="#" class="text-decoration-none text-white">Terms of Use  Policy</a>
+            </li>
+            <span class=" text-white">|</span>
+            <li>
+              <a href="#" class="text-decoration-none text-white">Sitemap</a>
+            </li>
+            <span class=" text-white">|</span>
+            <li>
+              <a href="#" class="text-decoration-none text-white">Blog</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-md-12">
+          <p class="text-center text-white mb-0">Copyright © <span id="year">2024</span>  All Rights Reserved by <a href="https://in.masticlubs.com/" class="text-white text-decoration-none">Masticlubs Network</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Footer -->
+  <!-- Modal On Page Load -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered ">
+          <div class="modal-content">
+              <div class="modal-body">
+                  <div class="pop_up_content text-center">
+                    <img src="images/hugs-love.gif" class="img-fluid text-center" width="30%"/>
+                    <p class="warning-para">Please read the following warning before continuing</p>
+                    <p class="mt-4 px-5 year_para"><b>I am over 18 years old</b> and I accept the viewing of explicit texts and images intended for an <b>adult audience</b>.</p>
+                    <p class="tnc_class">I have read and accept the<br> <a href="/terms-and-conditions/" class="text-decoration-none text-primary">Terms and Conditions</a></p>
+                  </div>
+              </div>
+              <div class="modal-footer d-block text-center border-0 mb-3">
+                  <button type="button" class="btn bttn btn-primary border-0 mb-3 w-50" id="agreeBtn" style="">ACCEPT</button><br>
+                  <a href="https://www.google.com" rel="noopener nofollow" data-dismiss="modal" class="text-center text-primary text-decoration-none">Decline</a>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- Modal On Page Load -->
+  <!-- Modal Search  -->
+  <div class="modal fade" id="buttonClickModal" tabindex="-1" aria-labelledby="buttonClickModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered ">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="buttonClickModalLabel"><i  class="fas fa-search text-clipped"></i> Search</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form method="POST" name="Search">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6  mb-3">
+                  <div class="form-group">
+                    <select class="form-control" id="service_category">
+                      <option>Call Girls</option>
+                      <option>Male Escorts</option>
+                      <option>Massage</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-md-6  mb-3">
+                  <div class="form-group">
+                    <select class="form-control" id="service_state">
+                      <option>Uttar Pradesh</option>
+                      <option>Delhi</option>
+                      <option>Haryana</option>
+                      <option>Bihar</option>
+                      <option>Goa</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6  mb-3">
+                  <div class="form-group">
+                    <select class="form-control" id="service_city">
+                      <option>Ghaziabad</option>
+                      <option>Lucknow</option>
+                      <option>Buxar</option>
+                      <option>Ambala</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-md-6  mb-3">
+                  <div class="form-group">
+                    <input class="form-control" type="text" placeholder="Looking for...">
+                  </div>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col-md-12">
+                  <button class="btn bttn btn-primary w-50 search_bttn" type="submit">Search</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal Search  -->
+
+  <!-- Modal Account  -->
+  <div class="modal fade" id="buttonClickModalSignup" tabindex="-1" aria-labelledby="buttonClickModalSignupLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered ">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="buttonClickModalSignupLabel"><i  class="fas fa-user text-clipped"></i> Get into Masticlubs!</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form method="POST" name="Search">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="gif_image d-flex justify-content-center">
+                    <img src="images/hugs-love.gif" class="img-fluid text-center" width="30%"/>
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <p class="text-center mt-2">Have an account yet? <br /><a href="login.html" class="btn bttn btn-primary w-50 mt-3 text-decoration-none">Login</a></p>
+                </div>
+                <div class="col-md-12  mb-2">
+                  <p class="text-center">Don't have an account yet? <br /> <a href="signup.html" class="btn bttn btn-primary w-50 mt-3 text-decoration-none">Signup</a></p>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal Acount  -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
