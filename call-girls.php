e5122f50b4d000d0a288d9853da19732cf43d66f
Amit Kushwaha <akkushwaha018@gmail.com>
2024-04-03 20:51:34 +0530

    banner added in call girls page

--- call-girls.php
+++ call-girls.php
@@ -1,3 +1,13 @@
 <?php include_once('./includes/header.php') ?>
-
+<section class="callgirl-banner">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="banner_content">
+            <button type="button" class="btn bttn btn-warning mt-4 text-center" data-bs-toggle="modal" data-bs-target="#buttonClickModal">Search Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 <?php include_once('./includes/footer.php') ?>
